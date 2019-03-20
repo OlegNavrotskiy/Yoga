0f913177ca0f88cf27bc054a56e2a47fb57dfbd1
Your Name <you@example.com>
2019-03-20 13:27:11 +0200

    Slider + Calc

--- js/script.js
+++ js/script.js
@@ -309,4 +309,105 @@ const inputsPhone = document.querySelectorAll('input[name="phone"]');
     [...inputsPhone].forEach(elem => onlyNumber(elem));
 
 
+// Slider
+let slideIndex = 1,
+    slides = document.querySelectorAll('.slider-item'),
+    prev = document.querySelector('.prev'),
+    next = document.querySelector('.next'),
+    dotsWrap = document.querySelector('.slider-dots'),
+    dots = document.querySelectorAll('.dot');
+
+showSlides(slideIndex);
+
+function showSlides(n) {
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  slides.forEach((item) => item.style.display = 'none');
+  dots.forEach((item) => item.classList.remove('dot-active'));
+
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].classList.add('dot-active');
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+prev.addEventListener('click', function() {
+  plusSlides(-1);
+});
+next.addEventListener('click', function() {
+  plusSlides(1);  
+});
+
+dotsWrap.addEventListener('click', function(event) {
+  for(let i = 0; i < dots.length + 1; i++) {
+    if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+      currentSlide(i);
+    }
+  }
+});
+
+
+//Calc
+let persons = document.querySelectorAll('.counter-block-input')[0],
+    restDays = document.querySelectorAll('.counter-block-input')[1],
+    place = document.getElementById('select'),
+    totalValue = document.getElementById('total'),
+    personSum = 0,
+    daysSum = 0,
+    total = 0;
+
+    totalValue.innerHTML = 0;
+
+persons.addEventListener('change', function() {
+  personSum = +this.value;
+  total = (personSum + daysSum) * 4000;
+
+  if (restDays.value == '' || persons.value == '') {
+    totalValue.innerHTML = 0;
+  } else {
+    totalValue.innerHTML = total;
+  }
+});
+
+restDays.addEventListener('change', function() {
+  daysSum = +this.value;
+  total = (personSum + daysSum) * 4000;
+
+  if (persons.value == '' || restDays.value == '') {
+    totalValue.innerHTML = 0;
+  } else {
+    totalValue.innerHTML = total;
+  }
+});
+
+place.addEventListener('change', function() {
+  if (restDays.value == '' || persons.value == '') {
+    totalValue.innerHTML = 0;
+  } else {
+    let a = total;
+    totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+  }
+});
+
+// Ввод только цифр в калькуляторе
+const inputsCalc = document.querySelectorAll('.counter-block-input');
+
+    function onlyNumberCalc(input) {
+        input.onkeyup = function () {
+            return (this.value = this.value.replace(/[^0-9]/g, ""));
+        };
+    }
+    [...inputsCalc].forEach(elem => onlyNumberCalc(elem));
+
 }); //конец DOMContentLoaded
